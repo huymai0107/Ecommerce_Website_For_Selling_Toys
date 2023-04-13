74979e98f001bdc47cf6d7834678ff96e4e0618d
Huy Mai <huymai172002@gmail.com>
2023-04-13 14:17:23 +0700

    adding model

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const dotenv = require('dotenv')
+const customer = require('./routes/customer')
+const auth = require('./routes/auth')
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use('/customer', customer)
+app.use('/auth', auth)
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log('Connected'))
+    .catch(err => console.error('Unconnected', err))
+
