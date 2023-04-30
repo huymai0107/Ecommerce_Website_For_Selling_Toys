b0bb1c9749c99becf442289be929837ff5ac6b10
Truong Hung Quan <quantruongh571@gmail.com>
2023-04-30 11:58:41 +0700

    Add home page, login and sign up
    Add components like Navbar, Announcement, Footer
    Add pages like Sign In and Sign Up page with util to upload profile picture

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Announcement from './component/Announcement/Announcement';
+import Footer from './component/Footer/Footer';
+import Navbar from './component/Navbar/Navbar';
+import { Outlet } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Announcement/> 
+      <Navbar/>
+      <main className="pt-16 white min-w-h-[calc(100vh)]">
+      <Outlet/> 
+      </main>
+      <Footer/> 
     </div>
   );
 }
