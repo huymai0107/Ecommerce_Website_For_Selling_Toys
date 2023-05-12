bf0912950c333a0a3e1cad9ccfb8b1387f1bafb3
Huy Mai <huymai172002@gmail.com>
2023-05-12 14:05:31 +0700

    update

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -16,13 +16,11 @@ function Signup() {
     setShowConfirmPassword(preve => !preve)
   }
 
-  const [data, setData] = useState({
-    firstName: "",
-    lastName: "",
+  const [
+    data, setData] = useState({
+    username: "",
     email: "",
     password: "",
-    confirmPassword: "",
-    image : ""
   });
   console.log(data);
 
@@ -35,25 +33,13 @@ function Signup() {
       };
     });
   };
-  const handleUploadProfileImage = async(e)=>{
-    const data = await ImagetoBase64(e.target.files[0])
-    console.log(data)
 
-    setData((preve)=>{
-        return{
-          ...preve,
-          image : data
-        }
-    })
-
-}
-console.log(process.env.REACT_APP_SERVER_DOMAIN)
   const handleSubmit = async(e) => {
     e.preventDefault()
-    const{firstName, email,password,confirmPassword} = data 
-    if(firstName && email && password && confirmPassword){
-      if(password === confirmPassword){
-        const fetchData = await fetch(`${process.env.REACT_APP_SERVER_DOMAIN}/signup`,{
+    const{username, email,password} = data 
+    if(username && email && password){
+      if(password){
+        const fetchData = await fetch("http://localhost:8080/auth/register",{
           method: "POST",
           headers:{
             "content-type": "application/json"
@@ -65,7 +51,7 @@ console.log(process.env.REACT_APP_SERVER_DOMAIN)
         // alert(dataRes.message)
      
 
-        navigate("/login")
+        // navigate("/login")
       }
       else{
         alert("Password and confirm password not the same")
@@ -80,22 +66,17 @@ console.log(process.env.REACT_APP_SERVER_DOMAIN)
     <div className='w-full max-w-sm bg-white m-auto flex items-center flex-col p-2'> 
         {/* <h1 className='text-center text-2xl font-bold'> Signup</h1> */}
         <div className="w-20 h-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto relative ">
-          <img src={data.image ? data.image :  loginSignupImage} alt='' className="w-full h-full" />
 
           <label htmlFor="profileImage">
             <div className="absolute bottom-0 h-1/3  bg-slate-500 bg-opacity-50 w-full text-center cursor-pointer">
               <p className="text-sm p-1 text-white">Upload</p>
             </div>
-            <input type={"file"} id="profileImage" accept="image/*" className="hidden" onChange={handleUploadProfileImage}/>
           </label>
         </div>
 
         <form className='w-full py-3 flex flex-col' onSubmit={handleSubmit}>
-          <label htmlFor='firstName'>First Name</label>
-          <input type={'text'} id='firstName' name='firstName' className='mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded' value={data.firstName}  onChange={handleOnChange}/>
-
-          <label htmlFor='lastName'>Last Name</label>
-          <input type={'text'} id='lastName' name='lastName' className='mt-1  mb-2 w-full bg-slate-200 px-2 py-1 rounded' value={data.lastName}  onChange={handleOnChange} />
+          <label htmlFor='username'>First Name</label>
+          <input type={'text'} id='username' name='username' className='mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded' value={data.username}  onChange={handleOnChange}/>
            
           <label htmlFor='email'>Email </label>
           <input type={'email'} id='email' name='email' className='mt-1 mb-2  w-full bg-slate-200 px-2 py-1 rounded' value={data.email}  onChange={handleOnChange}/>
@@ -106,11 +87,11 @@ console.log(process.env.REACT_APP_SERVER_DOMAIN)
           <span className='flex text-xl'onClick={handleShowPassword}>{ showPassword ?<BiShow/> :<BiHide/>}</span>
           </div>
 
-          <label htmlFor='confirmPassword'>Confirm password </label>
+          {/* <label htmlFor='confirmPassword'>Confirm password </label>
           <div className='flex px-2 py-1 mt-1 bg-slate-200 mb-2 rounded outline-2 focus-within:outline-blue-300'> 
-          <input type={ showConfirmPassword ? 'text':'password'} id='confirmppassword' name='confirmPassword' className='w-full bg-slate-200 border-none outline-none' value={data.confirmPassword}  onChange={handleOnChange}/>
+          <input type={ showConfirmPassword ? 'text':'password'} id='confirmppassword' name='confirmPassword' className='w-full bg-slate-200 border-none outline-none'  onChange={handleOnChange}/>
           <span className='flex text-xl'onClick={handleShowConfirmPassword}>{ showConfirmPassword ?<BiShow/> :<BiHide/>}</span>
-          </div>
+          </div> */}
           <button className="w-full max-w-[150px] m-auto  bg-red-500 hover:bg-red-600 cursor-pointer  text-white text-xl font-medium text-center py-1 rounded-full mt-4" type='submit'>
             Sign up
           </button>
