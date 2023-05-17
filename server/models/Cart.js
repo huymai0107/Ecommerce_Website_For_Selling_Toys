9cbf8247418d9943ed7132dfc91d8e28948edcf3
Truong Hung Quan <quantruongh571@gmail.com>
2023-05-17 21:10:28 +0700

    Update Cart.js

--- server/models/Cart.js
+++ server/models/Cart.js
@@ -7,19 +7,20 @@ const cartSchema = new mongoose.Schema({
     unique: true,
     required: true
   },
-  items: [{
-    productId: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Product',
-      required: true
-    },
-    quantity: {
-      type: Number,
-      required: true,
-      default: 1
-    }
-  }
-    ]
+  items: {
+    type: [{
+      productId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Product',
+        required: true
+      },
+      quantity: {
+        type: Number,
+        required: true,
+        default: 1
+      }
+    }],
+    default: []}
 });
 
 module.exports = mongoose.model('Cart', cartSchema);
